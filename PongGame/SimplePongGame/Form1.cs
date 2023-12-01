a90d4d77c97a2e38cff3fd1bda11afbc75b8536a
Jordan Olguin <jdolguin62@gmail.com>
2023-11-30 18:26:41 -0700

    created PictureBoxes for paddles and Ball

--- PongGame/SimplePongGame/Form1.cs
+++ PongGame/SimplePongGame/Form1.cs
@@ -6,6 +6,17 @@ namespace SimplePongGame
 {
     public partial class Form1 : Form1
     {
-        //Pong game variables
+        private const int PaddleWidth = 20;
+        private const int PaddleHeight = 100;
+        private const int BallSize = 20;
+
+        private int paddleSpeed = 5;
+        private int ballSpeed = 5;
+        private int ballXSpeed = 1;
+        private int ballYSpeed = 1;
+
+        private PictureBox paddle1;
+        private PictureBox paddle2;
+        private PictureBox ball;
     }
 }
