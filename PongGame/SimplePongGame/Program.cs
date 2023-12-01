5e2a0049d39d4dfa833c3ea047966dfeafeab87a
Jordan Olguin <jdolguin62@gmail.com>
2023-11-30 18:14:10 -0700

    modified Program.cs

--- PongGame/SimplePongGame/Program.cs
+++ PongGame/SimplePongGame/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimplePongGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
