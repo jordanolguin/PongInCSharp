d61356febadd33214832c2c228c12e38a79643d7
Jordan Olguin <jdolguin62@gmail.com>
2023-11-30 18:17:08 -0700

    created game form

---
+++ PongGame/SimplePongGame/Form1.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimplePongGame
+{
+    public partial class Form1 : Form1
+    {
+        //Pong game variables
+    }
+}
